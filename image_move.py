6fe471f7711cb13c15ca0d5455c82bd97c650dca
wongpatrick <wp.patrickwong@gmail.com>
2023-08-14 09:52:04 -0500

    Added rename functionality to fix overlapping name issue

--- image_move.py
+++ image_move.py
@@ -1,30 +1,39 @@
+"""
+This script is essentially for sorting out images 
+I would need to crop for either 16:9 or 9:16
+"""
+
 import os
 import shutil
 import numpy as np
 import cv2
 
 search_path = u"H:\Downloads\\wallpaper"
+NEW_PATH = u"H:\Downloads\organized_wallpaper\\"
 
-images = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(search_path)) for f in fn if '.jpg' in f.lower()]
+images = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(search_path)) for f in fn if '.jpg' or '.png' in f.lower()]
 
 # os.mkdir('downloaded_images')
 
 for image in images:
-    print(image)
-
+    splitName = image.split('\\')
+    newName = splitName[-2] + ' - ' + splitName[-1]
+    splitName[-1] = newName
+    
     stream = open(image, "rb")
     bytes = bytearray(stream.read())
     numpyarray = np.asarray(bytes, dtype=np.uint8)
     im = cv2.imdecode(numpyarray, cv2.IMREAD_UNCHANGED)
     stream.close()
 
+    os.rename(image, '\\'.join(splitName))
+    image = '\\'.join(splitName)
+    
     new_path = ""
     height, width, channel = im.shape
     if width > height:
-        print("Potentially 16:9")
-        new_path = 'downloaded_images/16x9'
+        new_path = NEW_PATH + '16x9'
     else:
-        print("Potentially 9:16")
-        new_path = 'downloaded_images/9x16'
+        new_path = NEW_PATH + '9x16'
     
     shutil.move(image, new_path)
