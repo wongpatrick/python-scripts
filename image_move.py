e17f275c51f573bc17cfef7b93f1a50760c4b88c
wongpatrick <wp.patrickwong@gmail.com>
2023-08-16 09:59:03 -0500

    fix constant standards

--- image_move.py
+++ image_move.py
@@ -8,10 +8,10 @@ import shutil
 import numpy as np
 import cv2
 
-search_path = u"H:\Downloads\\wallpaper"
+SEARCH_PATH = u"H:\Downloads\\wallpaper"
 NEW_PATH = u"H:\Downloads\organized_wallpaper\\"
 
-images = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(search_path)) for f in fn if '.jpg' or '.png' in f.lower()]
+images = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(SEARCH_PATH)) for f in fn if '.jpg' or '.png' in f.lower()]
 
 # os.mkdir('downloaded_images')
 
@@ -19,23 +19,23 @@ for image in images:
     if os.stat(image).st_size > 10485760:
         continue
     splitName = image.split('\\')
-    newName = splitName[-2] + ' - ' + splitName[-1]
-    splitName[-1] = newName
+    splitName[-1] = splitName[-2] + ' - ' + splitName[-1]
     
     stream = open(image, "rb")
     bytes = bytearray(stream.read())
     numpyarray = np.asarray(bytes, dtype=np.uint8)
     im = cv2.imdecode(numpyarray, cv2.IMREAD_UNCHANGED)
     stream.close()
-
+    
+    ## Renaming image to handle the case where there is a same name in the original image
     os.rename(image, '\\'.join(splitName))
     image = '\\'.join(splitName)
     
-    new_path = ""
+    new_path = NEW_PATH
     height, width, channel = im.shape
     if width > height:
-        new_path = NEW_PATH + '16x9'
+        new_path += '16x9'
     else:
-        new_path = NEW_PATH + '9x16'
+        new_path += '9x16'
     
     shutil.move(image, new_path)
