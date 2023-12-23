65e5fe0ba8eeef49d4d2d52c36217ace10036017
Patrick Wong <wp.patrickwong@gmail.com>
2023-12-23 04:19:47 -0500

    Added logic to search, locate average face, and throw exception when no face is found

--- image_anime_crop.py
+++ image_anime_crop.py
@@ -4,8 +4,9 @@ from PIL import Image
 import sys
 import os.path
 import shutil
+import numpy as np
 
-
+SEARCH_PATH = u"H:\Downloads\organized_wallpaper\\"
 NEW_PATH = u"H:\Downloads\cropped_wallpaper\\"
 
 def detect(filename, cascade_file = "./lbpcascade_animeface.xml"):
@@ -13,6 +14,16 @@ def detect(filename, cascade_file = "./lbpcascade_animeface.xml"):
         raise RuntimeError("%s: not found" % cascade_file)
 
     cascade = cv2.CascadeClassifier(cascade_file)
+
+    splitName = filename.split('\\')
+    stream = open(filename, "rb")
+    bytes = bytearray(stream.read())
+    numpyarray = np.asarray(bytes, dtype=np.uint8)
+    im = cv2.imdecode(numpyarray, cv2.IMREAD_UNCHANGED)
+    stream.close()
+    os.rename(filename, '\\'.join(splitName))
+    filename = '\\'.join(splitName)
+    
     with Image.open(filename) as input_image:
         print("Working on "+ filename)
         new_path = NEW_PATH
@@ -31,12 +42,21 @@ def detect(filename, cascade_file = "./lbpcascade_animeface.xml"):
             cv2.rectangle(input_detection, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
         if len(faces) > 0:
-            x,y,w,h = faces[0]  # Assuming you want to focus on the first detected face
-            x_center = (x + w) // 2
-            y_center = (y + h) // 2
+            x_arr = []
+            y_arr = []
+            for face in faces:
+                x,y,w,h = face
+                x_arr.append((x + w) // 2)
+                y_arr.append((y + h) // 2)
+            x_center = np.average(x_arr)
+            y_center = np.average(y_arr)
+            # x,y,w,h = faces[0]  # Assuming you want to focus on the first detected face
+            # x_center = (x + w) // 2
+            # y_center = (y + h) // 2
         else:
-            x_center = input_image.width // 2
-            y_center = input_image.height // 2
+            # x_center = input_image.width // 2
+            # y_center = input_image.height // 2
+            raise Exception("Found no face")
 
         # Determine if 9x16 or 16x9
         if input_image.height > input_image.width:
@@ -70,12 +90,17 @@ def detect(filename, cascade_file = "./lbpcascade_animeface.xml"):
         cropped_image = input_image.crop(crop_box)
         cropped_image.save(filename)
         shutil.move(filename, new_path)
-        # cv2.imshow("AnimeFaceDetect", input_image)
-        # cv2.waitKey(0)
-        # cv2.imwrite("out.png", input_image)
 
-if len(sys.argv) != 2:
-    sys.stderr.write("usage: detect.py <filename>\n")
-    sys.exit(-1)
-    
-detect(sys.argv[1])
+# if len(sys.argv) != 2:
+#     sys.stderr.write("usage: detect.py <filename>\n")
+#     sys.exit(-1)
+
+# detect(sys.argv[1])
+        
+images = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(SEARCH_PATH)) for f in fn if '.jpg' or '.png' in f.lower()]
+
+for image in images:
+    try:
+        detect(image)
+    except:
+        print(image + " failed")
